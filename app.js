080039782adac88e2249c1da4722475a79519875
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-01-27 02:20:08 +1300

    renamed the OAuth, changed the URL

--- app.js
+++ app.js
@@ -17,12 +17,15 @@ searchUser.addEventListener('keyup', (e) => {
             .then(data => {
                 if (data.profile.message === 'Not Found') {
                     //show alert
+                    ui.showAlert('User not found', 'alert alert-danger')
                 } else {
                     //show profile
                     ui.showProfile(data.profile)
+                    ui.showRepos(data.repos)
                 }
             })
     } else {
         //clear profile
+        ui.clearProfile()
     }
 })
--- github.js
+++ github.js
@@ -2,6 +2,8 @@ class Github {
   constructor() {
     this.client_id = "30e15e468c61e56dd3a9";
     this.client_secret = "d8019fb3b18eaceb34cf69ce47f00342d911bc1c";
+    this.repos_count = 10;
+    this.repos_sort = 'created: asc'
   }
 
   async getUser(user) {
@@ -9,10 +11,16 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoRes = await fetch(`
+    https://api.github.com/users/${user}/repos?per_page=${this.client_id}&sort=created:asc&client_id=${this.client_id}&client_secret=${this.client_secret}
+    `)
+
     const profile = await profileRes.json(); 
+    const repos = await repoRes.json(); 
 
     return {
-        profile
+        profile,
+        repos
     }
   }
 }
